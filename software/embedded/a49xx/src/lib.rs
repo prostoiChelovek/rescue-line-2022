631c50546c59a3a467114bf28ba938a587bbf6d0
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-03-20 18:27:26 +0500

    revert an attempt to implement proper timing handling for set_direciton
    This reverts commits e2d5f4..5a9442

--- software/embedded/a49xx/src/lib.rs
+++ software/embedded/a49xx/src/lib.rs
@@ -6,60 +6,36 @@ use rust_fsm::*;
 
 pub type SpawnFn = fn() -> ();
 
-pub struct StepperTimings {
-    pub pulse_width: MicrosDurationU32,
-}
-
-impl Default for StepperTimings {
-    fn default() -> Self {
-        Self {
-            pulse_width: 1_u32.micros()
-        }
-    }
-}
-
 state_machine! {
     derive(Debug, PartialEq)
     StepperState(Idle)
 
-    Idle(Start) => StartStepping,
-    StartStepping(Started) => Stepping,
+    Idle(Start) => StartStepHigh,
+
+    StartStepHigh(PulseStart) => StepHigh,
+    StepHigh(PulseEnd) => StartStepLow,
+
+    StartStepLow(PulseStart) => StepLow,
+    StepLow(PulseEnd) => StartStepHigh,
+
     Idle(Stop) => Idle,
-
-    Stepping(Stop) => StopStepping,
-    StopStepping(Stopped) => Idle
-}
-
-state_machine! {
-    derive(Debug, PartialEq)
-    StepState(Idle)
-
-    Idle(Start) => StartHigh,
-
-    StartHigh(PulseStart) => High,
-    High(PulseEnd) => StartLow,
-
-    StartLow(PulseStart) => Low,
-    Low(PulseEnd) => StartHigh,
-
-    StartHigh(Stop) => Idle,
-    High(Stop) => StartLow [Stop],
-    StartLow(Stop) => Idle,
-    Low(Stop) => Idle,
+    StartStepHigh(Stop) => Idle,
+    StepHigh(Stop) => StartStepLow [Stop],
+    StartStepLow(Stop) => Idle,
+    StepLow(Stop) => Idle,
 }
 
 pub struct A49xx<S, D>
 where
     S: OutputPin,
     D: OutputPin, {
-    pub timings: StepperTimings,
+    pub pulse_width: MicrosDurationU32,
     step_delay: Option<MicrosDurationU32>,
 
     step: S,
     dir: D,
 
     state_machine: StateMachine<StepperState>,
-    step_state_machine: StateMachine<StepState>,
 
     spawn_fn: SpawnFn
 }
@@ -70,14 +46,10 @@ where
     D: OutputPin, {
     pub fn new(step: S, dir: D, spawn_fn: SpawnFn) -> Self {
         Self {
-            timings: Default::default(),
-
+            pulse_width: 2_u32.micros(),
             step_delay: None,
             step, dir,
-
             state_machine: StateMachine::<StepperState>::new(),
-            step_state_machine: StateMachine::<StepState>::new(),
-
             spawn_fn
         }
     }
@@ -100,43 +72,23 @@ where
             return None;
         }
 
-        // TODO: control flow is fucked up
         match *self.state_machine.state() {
-            StepperStateState::Idle => { None },
-            StepperStateState::StartStepping => {
-                self.step_state_machine.consume(&StepStateInput::Start).unwrap();
-                self.state_machine.consume(&StepperStateInput::Started).unwrap();
-                self.update()
+            StepperStateState::Idle => { None }
+            StepperStateState::StartStepHigh => {
+                self.step.set_high().ok();
+                self.state_machine.consume(&StepperStateInput::PulseStart).unwrap();
+
+                Some(self.pulse_width)
             },
-            StepperStateState::Stepping => {
-                let state = match *self.step_state_machine.state() {
-                    StepStateState::Idle => { StepperStateInput::Stopped },
-                    StepStateState::StartHigh => {
-                        self.step.set_high().ok();
-                        self.step_state_machine.consume(&StepStateInput::PulseStart).unwrap();
+            StepperStateState::StartStepLow => {
+                self.step.set_low().ok();
+                self.state_machine.consume(&StepperStateInput::PulseStart).unwrap();
 
-                        return Some(self.timings.pulse_width);
-                    },
-                    StepStateState::StartLow => {
-                        self.step.set_low().ok();
-                        self.step_state_machine.consume(&StepStateInput::PulseStart).unwrap();
-
-                        return Some(self.step_delay.unwrap());
-                    },
-                    StepStateState::High | StepStateState::Low => {
-                        self.step_state_machine.consume(&StepStateInput::PulseEnd).unwrap();
-
-                        return self.update();
-                    },
-                };
-                self.state_machine.consume(&state).unwrap();
-                self.update()
+                Some(self.step_delay.unwrap())
             },
-            StepperStateState::StopStepping => {
-                self.step_state_machine.consume(&StepStateInput::Stop).unwrap();
-                if self.step_state_machine.state() == &StepStateState::Idle {
-                    self.state_machine.consume(&StepperStateInput::Stopped).unwrap();
-                }
+            StepperStateState::StepHigh | StepperStateState::StepLow => {
+                self.state_machine.consume(&StepperStateInput::PulseEnd).unwrap();
+
                 self.update()
             }
         }
