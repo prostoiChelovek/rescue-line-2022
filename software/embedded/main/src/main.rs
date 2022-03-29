3b714bb5c483df3d64ca00d8613be688ba472e53
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-03-29 22:33:53 +0500

    lay out the foundation for implementing interfacing over serial

--- software/embedded/main/src/main.rs
+++ software/embedded/main/src/main.rs
@@ -8,16 +8,19 @@ mod app {
     use rtt_target::{rtt_init_print, rprintln};
 
     use stm32f4xx_hal::{
-        prelude::*, pac,
+        prelude::*, pac, pac::{USART2, NVIC, Interrupt},
         timer::{monotonic::MonoTimer, Timer},
         gpio::{
             gpioa::PA10, gpiob::{PB3, PB10, PB4},
             Output, PushPull
         },
+        serial, serial::{config::Config, Event, Serial},
     };
     use fugit::{RateExtU32, HertzU32};
 
     use stepper::{Stepper, StepperDireciton};
+
+    use interfacing::Interfacing;
 
     #[monotonic(binds = TIM2, default = true)]
     type MicrosecMono = MonoTimer<pac::TIM2, 1_000_000>;
@@ -25,7 +28,10 @@ mod app {
     #[shared]
     struct Shared {
         left_stepper: Stepper<PA10<Output<PushPull>>, PB4<Output<PushPull>>>,
-        right_stepper: Stepper<PB3<Output<PushPull>>, PB10<Output<PushPull>>>
+        right_stepper: Stepper<PB3<Output<PushPull>>, PB10<Output<PushPull>>>,
+        interfacing: Interfacing,
+        serial_tx: serial::Tx<USART2>,
+        serial_rx: serial::Rx<USART2>,
     }
 
     #[local]
@@ -44,13 +50,13 @@ mod app {
         let (gpioa, gpiob) = (ctx.device.GPIOA.split(), ctx.device.GPIOB.split());
 
         let mut en = gpioa.pa9.into_push_pull_output();
-        en.set_low();
+        en.set_high();
         
         let left_stepper = {
             let (step, dir) = (gpioa.pa10.into_push_pull_output(), gpiob.pb4.into_push_pull_output());
             let mut stepper = Stepper::new(step, dir, || test::spawn().unwrap());
             stepper.set_direciton(StepperDireciton::CounterClockwise);
-            stepper.set_speed(400_u32.Hz());
+            //stepper.set_speed(400_u32.Hz());
             stepper
         };
 
@@ -58,20 +64,39 @@ mod app {
             let (step, dir) = (gpiob.pb3.into_push_pull_output(), gpiob.pb10.into_push_pull_output());
             let mut stepper = Stepper::new(step, dir, || right::spawn().unwrap());
             stepper.set_direciton(StepperDireciton::CounterClockwise);
-            stepper.set_speed(100_u32.Hz());
+            //stepper.set_speed(100_u32.Hz());
             stepper
         };
 
-
-
         let mono = Timer::new(ctx.device.TIM2, &clocks).monotonic();
+
+        let tx = gpioa.pa2.into_alternate();
+        let rx = gpioa.pa3.into_alternate();
+        let mut serial = Serial::new(
+            ctx.device.USART2,
+            (tx, rx),
+            Config::default().baudrate(interfacing::BAUD_RATE.bps()),
+            &clocks,
+            )
+            .unwrap();
+	    serial.listen(Event::Rxne);
+	
+	    // Enable interrupt
+	    NVIC::unpend(Interrupt::USART2);
+	    unsafe {
+	        NVIC::unmask(Interrupt::USART2);
+	    }
+	
+	    let (serial_tx, serial_rx) = serial.split();
 
         //change_speed::spawn().ok();
 
         (
             Shared {
                 left_stepper,
-                right_stepper
+                right_stepper,
+                interfacing: Interfacing::new(),
+                serial_tx, serial_rx,
             },
             Local {
                 speed: 400_u32.Hz(),
@@ -119,6 +144,20 @@ mod app {
             let next_delay = stepper.update();
             if let Some(next_delay) = next_delay {
                 right::spawn_after(next_delay).ok();
+            }
+        });
+    }
+
+    #[task(binds = USART2, shared = [serial_rx, serial_tx)]
+    fn uart_rx(cx: uart_rx::Context) {
+        cx.shared.serial_rx.lock(|rx| {
+            match rx.read() {
+                Ok(_byte) => {
+
+                },
+                Err(_e) => {
+                    // TODO
+                }
             }
         });
     }
