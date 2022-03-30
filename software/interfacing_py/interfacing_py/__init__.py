839f2a07640e31623e102c9b1560bd45301ec079
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-03-30 19:44:21 +0500

    remove inheritance from Interfacing and make it a field

--- software/interfacing_py/interfacing_py/__init__.py
+++ software/interfacing_py/interfacing_py/__init__.py
@@ -6,12 +6,12 @@ import logging
 from .interfacing_py import Interfacing, Command, CommandId, SetSpeedParams, PyCommand, MessageBuffer
 
 
-class InterfacingManager(Interfacing):
+class InterfacingManager:
     def __init__(self, port: str) -> None:
-        super().__init__()
+        self._interfacing = Interfacing()
 
         self._logger = logging.Logger(__name__)
-        self._serial = aioserial.AioSerial(port, baudrate=self.BAUD_RATE)
+        self._serial = aioserial.AioSerial(port, baudrate=self._interfacing.BAUD_RATE)
         self._command_futures: Dict[CommandId, asyncio.Future] = {}
 
         asyncio.create_task(self._updater())
@@ -21,12 +21,12 @@ class InterfacingManager(Interfacing):
         while True:
             try:
                 byte = await self._serial.read_async(size = 1)
-                self.handle_received_byte(byte)
+                self._interfacing.handle_received_byte(byte)
 
                 for handle, future in list(self._command_futures.items()):
-                    if self.is_finished(handle):
+                    if self._interfacing.is_finished(handle):
                         future.set_result(None)
-                        self.ack_finish(handle)
+                        self._interfacing.ack_finish(handle)
                         del self._command_futures[handle]
             except Exception:
                 self._logger.exception("Error while running update loop")
@@ -34,7 +34,7 @@ class InterfacingManager(Interfacing):
     async def _sender(self):
         while True:
             try:
-                to_send = self.get_message_to_send()
+                to_send = self._interfacing.get_message_to_send()
                 if to_send is not None:
                     print(list(to_send))
                     await self._serial.write_async(bytes(to_send))
@@ -43,7 +43,7 @@ class InterfacingManager(Interfacing):
                 self._logger.exception("Error while running send loop")
 
     def execute(self, cmd: PyCommand) -> asyncio.Future:
-        handle = super().execute(cmd)
+        handle = self._interfacing.execute(cmd)
         future = asyncio.get_event_loop().create_future()
         self._command_futures[handle] = future
         return self._command_futures[handle]
