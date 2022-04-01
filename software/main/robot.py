24a6883e7620308aa469fb606862d7feb75d516a
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-01 21:33:53 +0500

    try to stop the robot when shutdown requested

--- software/main/robot.py
+++ software/main/robot.py
@@ -2,6 +2,8 @@ import asyncio
 import threading
 import concurrent.futures
 import time
+from typing import Optional
+import logging
 
 from interfacing_py import InterfacingManager, PyCommand, Command, SetSpeedParams
 
@@ -21,7 +23,12 @@ class Robot:
 
         self._interfacing = interfacing_future.result(timeout=1)
 
-    def stop(self):
+    def shutdown(self):
+        try:
+            self.stop(timeout=1)
+        except concurrent.futures.TimeoutError:
+            logging.error("Cannot stop the robot")
+
         self._loop.call_soon_threadsafe(self._interfacing.stop)
         time.sleep(0.1)
         self._loop.call_soon_threadsafe(self._loop.stop)
@@ -34,15 +41,22 @@ class Robot:
         interfacing.set_result(InterfacingManager(SERIAL_PORT, loop))
         loop.run_forever()
 
-    def set_speed(self, left: float, right: float):
-        params = SetSpeedParams(self._to_steps(left), self._to_steps(right))
-        
-        fut = asyncio.run_coroutine_threadsafe(
-                        self._run_command(PyCommand(Command.SetSpeed, params)),
-                        self._loop)
-        fut.result()
+    def set_speed(self, left: float, right: float, timeout: Optional[float] = None):
+        self._execute_command(PyCommand(Command.SetSpeed,
+                              SetSpeedParams(self._to_steps(left),
+                                             self._to_steps(right))),
+                              timeout)
 
-    async def _run_command(self, cmd: PyCommand):
+    def stop(self, timeout: Optional[float] = None):
+        self._execute_command(PyCommand(Command.Stop), timeout)
+
+    def _execute_command(self, cmd: PyCommand, timeout: Optional[float] = None):
+        fut = asyncio.run_coroutine_threadsafe(
+                        self._command_future_wrapper(cmd),
+                        self._loop)
+        fut.result(timeout=timeout)
+
+    async def _command_future_wrapper(self, cmd: PyCommand):
         await self._interfacing.execute(cmd)
 
     def _to_steps(self, speed: float) -> int:
