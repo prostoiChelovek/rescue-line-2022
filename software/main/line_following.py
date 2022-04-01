d176d37392503fb41ddbca54795501c696d184b7
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-02 00:01:55 +0500

    clamp the speed

--- software/main/line_following.py
+++ software/main/line_following.py
@@ -7,6 +7,7 @@ from vision import colors, line
 
 FOLLOWING_SPEED = 0.8 # rps
 LINE_TARGET_X = 320
+MAX_SPEED = 0.2
 
 
 def get_line_offset(img) -> int:
@@ -15,10 +16,14 @@ def get_line_offset(img) -> int:
         return line_x - LINE_TARGET_X
 
 
+def clamp_speed(val: float) -> float:
+    return min(MAX_SPEED, max(-MAX_SPEED, val))
+
+
 class LineFollower:
     def __init__(self) -> None:
-        self._current_speed: Tuple[float, float]
         self._pid = PID(Kp=1.0, Ki=0.0, Kd=0.0, setpoint=0.0,
+        self._current_speed: Tuple[float, float] = None
                         output_limits=(-FOLLOWING_SPEED, FOLLOWING_SPEED))
 
     def update(self, img) -> Tuple[float, float]:
@@ -27,6 +32,6 @@ class LineFollower:
         else:
             offset = get_line_offset(img)
             correction = self._pid(offset)
-            self._current_speed = (self._current_speed[0] + correction,
-                                   self._current_speed[1] - correction)
+            self._current_speed = (clamp_speed(self._current_speed[0] - correction),
+                                   clamp_speed(self._current_speed[1] + correction))
         return self._current_speed
