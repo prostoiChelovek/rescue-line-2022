2ce0decc6da06ebcca350fea25d8027ca5344328
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-03 19:42:39 +0500

    implement detection of the green markers' position

--- software/vision/colors.py
+++ software/vision/colors.py
@@ -6,8 +6,20 @@ BLACK_COLOR_RANGE = (
         (0, 0, 0),
         (50, 50, 65)
         )
+GREEN_COLOR_RANGE = (
+        (45, 70, 0),
+        (65, 90, 20)
+        )
 
 
 def find_black(img):
-    mask = cv.inRange(img, *BLACK_COLOR_RANGE)
+    return find_color(img, BLACK_COLOR_RANGE)
+
+
+def find_green(img):
+    return find_color(img, GREEN_COLOR_RANGE)
+
+
+def find_color(img, range):
+    mask = cv.inRange(img, *range)
     return clean_mask(mask)
--- software/vision/intersection.py
+++ software/vision/intersection.py
@@ -1,24 +1,54 @@
+from enum import Enum
+from typing import Tuple
+
 import numpy as np
 import cv2.cv2 as cv
 
-TEST_IMAGE = "./intersection-images/0.jpg"
+from .colors import find_green
+
+TEST_IMAGE = "./vision/images/intersection/0.jpg"
 
 
-def filter_green(img):
-    b, g, r = cv.split(img)
+class MarkersPosition(Enum):
+    NONE = 0
+    LEFT = 1
+    RIGHT = 2
+    BOTH = 3
 
-    img[np.where(b >= 140)] = 0
-    img[np.where(r >= 50)] = 0
-    img[np.where(g < 95)] = 0
+
+def find(img, line_x: int, black_window: Tuple[int, int]) -> MarkersPosition:
+    green_window = img[black_window[0]:black_window[1],:]
+    left_green = np.flip(green_window[:,:line_x], axis=1)
+    right_green = green_window[:,line_x:]
+
+    has_green = [np.any(x > 0) for x in (left_green, right_green)]
+    position_num = sum(x + i * x for i, x in enumerate(has_green))
+
+    return MarkersPosition(position_num)
+
 
 
 def main():
+    from . import line
+    from .colors import find_black
+
     img = cv.imread(TEST_IMAGE)
     img = cv.resize(img, (img.shape[1] // 4, img.shape[0] // 4))
-    cv.imshow("orig", img)
+    img = img[:230,:]
 
-    filter_green(img)
+    green = find_green(img)
+    black = find_black(img)
 
+    black_window_pos = line.get_window_pos(black)
+    if black_window_pos is None:
+        raise Exception("no window")
+
+    line_x = line.find(black, black_window_pos)
+
+    marker_pos = find(green, line_x, black_window_pos)
+    print(marker_pos)
+
+    img[:, line_x] = (255, 0, 0)
     cv.imshow("img", img)
 
     while cv.waitKey(0) != 27:  # esc
