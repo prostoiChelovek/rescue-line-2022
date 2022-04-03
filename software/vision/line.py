7158a49c4cf8644e2293e5d28d4f8b22866e0d8d
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-03 17:17:45 +0500

    implement selection of the best region to ignore stuff on the sides

--- software/vision/line.py
+++ software/vision/line.py
@@ -1,10 +1,13 @@
+import math
 from typing import Optional
+
 import numpy as np
 from cv2 import cv2 as cv
+import skimage.measure
 
 from .common import clean_mask
 
-TEST_IMAGE = "./images/0.jpg"
+TEST_IMAGE = "./vision/images/0.jpg"
 LINE_COLOR_RANGE = (
         (0, 0, 0),
         (50, 50, 65)
@@ -13,16 +16,27 @@ LINE_COLOR_RANGE = (
 WINDOW_HEIGHT = 3
 
 
+def segment_unconnected(img):
+    labels = skimage.measure.label(img)
+    return skimage.measure.regionprops(label_image=labels)
+
+
+def get_best_region(regions):
+    def region_score(region):
+        return math.sqrt(region.area) * 0.25 + region.centroid[1]
+    return max(regions, key=region_score, default=None)
+
+
 def find(mask) -> Optional[int]:
-    white_points = np.array(np.where(mask == 255)).T
-    if white_points.size == 0:
+    lowest_white_y = np.argwhere(mask)[-1][0]
+    window = mask[lowest_white_y - WINDOW_HEIGHT:lowest_white_y]
+
+    regions = segment_unconnected(window)
+    region = get_best_region(regions)
+    if region is None:
         return None
 
-    lowest_white = np.max(white_points[:, 0])
-
-    window = white_points[white_points[:, 0] > lowest_white - WINDOW_HEIGHT]
-    line_x = int(np.median(window, axis=0)[1])
-
+    line_x = int(region.centroid[1])
     return line_x
 
 
