a4de21a44edac1608f1f03a64b5aefd1aff467f1
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-03 18:12:41 +0500

    Revert "update the color range to match red for testing"
    This reverts commit adfbc738cf6938feee7c94fd152ed9389339d34c.

--- software/vision/colors.py
+++ software/vision/colors.py
@@ -3,8 +3,8 @@ from cv2 import cv2 as cv
 from .common import clean_mask
 
 BLACK_COLOR_RANGE = (
-        (20, 10, 85),
-        (55, 45, 120)
+        (0, 0, 0),
+        (50, 50, 65)
         )
 
 
