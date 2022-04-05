cb28c87a1c11cc6fbb8238f67133be2e4474cc89
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-04-05 12:05:29 +0500

    inline the line follower
    WIP

--- software/main/main.py
+++ software/main/main.py
@@ -1,13 +1,17 @@
 import time
-import logging, coloredlogs
+import logging
+import coloredlogs
 
 from cv2 import cv2 as cv
+import numpy as np
 
+from simple_pid import PID
+
+from vision import colors, line
 from vision.camera import BufferlessCapture
 from vision.intersection import MarkersPosition
 
 from .robot import Robot
-from .line_following import LineFollower
 from .intersections import IntersectionsHandler
 from .intersections import Action as IntersectionAction
 
@@ -19,6 +23,23 @@ coloredlogs.install(fmt="%(asctime)s - %(threadName)s - %(levelname)s - %(module
 
 LOOP_INTERVAL = 1 / 15
 
+FOLLOWING_SPEED = 500  # sps
+MAX_SPEED = 600
+LINE_TARGET_X = 228
+INTERSECTION_FILL_FRAC = 0.7
+
+
+def debug(line_x, img, mask):
+    if line_x is not None:
+        img[:, line_x] = (255, 0, 0)
+
+    cv.imshow("i", img)
+    cv.imshow("m", mask)
+
+
+def clamp_speed(val: float) -> float:
+    return int(min(MAX_SPEED, max(-MAX_SPEED, val)))
+
 
 class RobotController:
     def __init__(self) -> None:
@@ -26,29 +47,45 @@ class RobotController:
         self._cap = BufferlessCapture(0)
         self._is_following_line = True
 
+        self._pid = PID(Kp=20.0, Ki=0.0, Kd=0.0, setpoint=0.0,
+                        output_limits=(-FOLLOWING_SPEED * 2,
+                                       FOLLOWING_SPEED * 2))
+
     def loop(self):
-        follower = LineFollower()
         intersections = IntersectionsHandler()
+
         while True:
             start = time.time()
 
             frame = self._cap.read()
-            frame = cv.resize(frame, (frame.shape[1] // 2, frame.shape[0] // 2))
+            frame = cv.resize(frame,
+                              (frame.shape[1] // 2, frame.shape[0] // 2))
 
-            new_speed, black_win, line_x = follower.update(frame)
-            if new_speed is not None and self._is_following_line:
-                intersections.update(frame, line_x, black_win)
-                self._robot.set_speed(*map(lambda x: -x, new_speed))
-            else:
-                self._is_following_line = False
-                markers_pos = intersections.finish_scanning()
-                self._robot.set_speed(-800, -800)
-                time.sleep(2)
-                if markers_pos == MarkersPosition.LEFT:
-                    self._robot.set_speed(800, -800)
-                    time.sleep(2)
-                    self._robot.stop()
-                    break
+            cropped = frame[:(frame.shape[1] // 2 - 5), :]
+            mask = colors.find_black(cropped)
+
+            window_pos = line.get_window_pos(mask)
+            line_x = line.find(mask, window_pos)
+
+            debug(line_x, cropped, mask)
+
+            offset = frame.shape[1]  # TODO: handle it properly
+            if line_x is not None:
+                offset = line_x - LINE_TARGET_X
+
+            correction = self._pid(offset) or 0
+
+            if window_pos is not None:
+                window = mask[window_pos[0]:window_pos[1]]
+                window_area = window.shape[0] * window.shape[1]
+                filled_frac = np.count_nonzero(window) / window_area
+                if filled_frac >= INTERSECTION_FILL_FRAC:
+                    # TODO
+                    pass
+
+            new_speed = (clamp_speed(FOLLOWING_SPEED - correction),
+                         clamp_speed(FOLLOWING_SPEED + correction))
+            logging.debug(f"err: {offset} ; correction: {correction} ; new speed: {new_speed}")
 
             dt = time.time() - start
             delay = int((LOOP_INTERVAL - dt) * 1000)
