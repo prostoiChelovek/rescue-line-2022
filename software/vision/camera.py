53d07a484a8f63f401f0a6bebcb1a94fd8651704
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-05-09 12:17:16 +0500

    set capture resolution to avoid software rescaling

--- software/vision/camera.py
+++ software/vision/camera.py
@@ -4,6 +4,8 @@ import collections
 
 from cv2 import cv2 as cv
 
+CAPTURE_RESOLUTION = (320, 240)
+
 
 class BufferlessCapture(threading.Thread):
     def __init__(self, name):
@@ -11,6 +13,11 @@ class BufferlessCapture(threading.Thread):
 
         self._cap = cv.VideoCapture(name)
         self._frame_buff = collections.deque(maxlen=1)
+
+        self._cap.set(cv.CAP_PROP_FRAME_WIDTH, CAPTURE_RESOLUTION[0])
+        self._cap.set(cv.CAP_PROP_FRAME_HEIGHT, CAPTURE_RESOLUTION[1])
+        assert self._cap.get(cv.CAP_PROP_FRAME_WIDTH) == CAPTURE_RESOLUTION[0]
+        assert self._cap.get(cv.CAP_PROP_FRAME_HEIGHT) == CAPTURE_RESOLUTION[1]
 
         self.start()
 
