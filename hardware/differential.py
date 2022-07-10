d7d78b4c2616373475b410635d886f49651cf29c
prostoichelovek <i.prostoi.chelovek@yandex.ru>
2022-07-10 18:15:34 +0500

    create both halves of the assembly simultaneously

--- hardware/differential.py
+++ hardware/differential.py
@@ -62,61 +62,70 @@ def Slot(root: Optional[OpenSCADObject] = None) -> OpenSCADObject:
     return root
 
 
-def half(is_lower: bool):
-    def out_gear(is_lower: bool):
-        return Gear(modul=GEAR_MODULE, tooth_number=30,
-                    width=30, bore=8,
-                    pressure_angle=PRESSURE_ANGLE,
-                    helix_angle=25 * (1 if is_lower else -1),
-                    optimized=False)
+def out_gear():
+    return Gear(modul=GEAR_MODULE, tooth_number=30,
+                width=30, bore=8,
+                pressure_angle=PRESSURE_ANGLE,
+                helix_angle=25,
+                optimized=False)
 
-    def planet_gear(is_lower: bool):
-        return Gear(modul=GEAR_MODULE, tooth_number=10,
-                    width=30 + PLANET_GEARS_MESH_HEIGHT, bore=8,
-                    pressure_angle=PRESSURE_ANGLE,
-                    helix_angle=25 * (-1 if is_lower else 1),
-                    optimized=False)
+def planet_gear():
+    return Gear(modul=GEAR_MODULE, tooth_number=10,
+                width=30 + PLANET_GEARS_MESH_HEIGHT, bore=8,
+                pressure_angle=PRESSURE_ANGLE,
+                helix_angle=25,
+                optimized=False)
 
-    sun = out_gear(is_lower)
-    planets = [planet_gear(is_lower)] * NUM_PLANETS
 
-    if is_lower:
-        root = Slot()
-        planets_r = Slot()
-    else:
-        root = Slot(up(sun.width + OUT_GEARS_DISTANCE))
+def assembly():
+    lower_sun = out_gear()
+    upper_sun = out_gear()
+    upper_sun.helix_angle *= -1
 
-        planets_distance = sun.pitch_radius + planet_gear(True).pitch_radius 
-        planets_rotation = asin(planet_gear(True).pitch_radius / planets_distance)
-        planets_r = Slot(down(PLANET_GEARS_MESH_HEIGHT)(
-            rotate((0, 0, degrees(planets_rotation * 2)))
-            )
-            )
+    upper_r = Slot(up(upper_sun.width + OUT_GEARS_DISTANCE))
+    suns_mesh_r = Slot(rotate((0, 0, lower_sun.mesh_rotation)))
 
-    for i, p in enumerate(planets):
+    planets = union()
+
+    for i in range(NUM_PLANETS):
+        lower_planet = planet_gear()
+        lower_planet.helix_angle *= -1
+        upper_planet = planet_gear()
+
+        planets_r = Slot(forward(lower_sun.pitch_radius + lower_planet.pitch_radius))
+        planets_distance = upper_sun.pitch_radius + lower_planet.pitch_radius 
+        planets_rotation = asin(lower_planet.pitch_radius / planets_distance)
+        upper_planet_r = Slot(
+                              rotate((0, 0, degrees(planets_rotation * 2)))(
+                                  upper_r(
+                                      planets_r(
+                                          down(PLANET_GEARS_MESH_HEIGHT)
+                                      )
+                                  )
+                              )
+                              )
+        
         distance_from_bot = OUT_GEARS_DISTANCE - PLANET_GEARS_MESH_HEIGHT
-        torsion_compensation = p.torsion_angle * (distance_from_bot / p.width)
-        mesh_rotation = p.mesh_rotation + torsion_compensation
+        torsion_compensation = upper_planet.torsion_angle * (distance_from_bot / upper_planet.width)
+        upper_mesh_rotation = upper_planet.mesh_rotation + torsion_compensation
 
-        transform = Slot(rotate(360 / NUM_PLANETS * i)(
-                        forward(sun.pitch_radius + p.pitch_radius)))
+        planets += rotate(360 / NUM_PLANETS * i)(
+                          planets_r(lower_planet()),
+                          upper_planet_r(
+                                         rotate((0, 0, upper_mesh_rotation))(
+                                                upper_planet()
+                                               )
+                                        )
+                         )
 
-        planets_r += transform(rotate((0, 0, mesh_rotation if not is_lower else 0))( p()))
-
-    root += planets_r
-
-    root += rotate((0, 0, sun.mesh_rotation))(
-                sun()
-                )
-
-    return root
+    return union()(
+                   suns_mesh_r(lower_sun()),
+                   suns_mesh_r(upper_r(upper_sun())),
+                   planets)
 
 
 def main():
-    lower = half(True)
-    upper = half(False)
-
-    return lower + upper
+    return assembly()
 
 
 if __name__ == "__main__":
